7fc1a6f872ab55991e59f64cee1a7e8e5845ce42
minhnguyenbn99 <66963269+minhnguyenbn99@users.noreply.github.com>
2020-11-01 16:29:38 +0700

    Add files via upload

---
+++ City.java
@@ -0,0 +1,72 @@
+package tsp02;
+
+public class City {
+	    int x;
+	    int y;
+	    String cityName;
+	    static int count = 0;
+	    
+	    public City()
+	    {
+	        this.x = (int)(Math.random()*200);  
+	        this.y = (int)(Math.random()*200);   
+	        count++;
+	        this.cityName = "City " + Integer.toString(count);
+	    }
+	    
+	  
+	    public City(int x, int y)
+	    {
+	        this.x = x;
+	        this.y = y;
+	        count++;
+	        this.cityName = "City " + Integer.toString(count) ;
+	    }
+	    
+	   
+	    public int getX()
+	    {
+	        return this.x;
+	    }
+	    
+	 
+	    public int getY()
+	    {
+	        return this.y;
+	    }
+	    
+	    // Gets the distance 
+	    public double distanceTo(City city)
+	    {
+	        int xDistance = Math.abs(getX() - city.getX());
+	        int yDistance = Math.abs(getY() - city.getY());
+	        
+	        double distance = 0.0;
+	        distance = Math.sqrt( (xDistance*xDistance) + (yDistance*yDistance) );
+	        
+	        return distance;
+	    }
+	    
+	    public static boolean isEqual(City c1, City c2)
+	    {
+	      boolean b = false;
+	      
+	      if(c1.getX() == c2.getX() && c1.getY() == c2.getY())
+	         b = true;
+	      
+	      return b;
+	    }
+	      
+	    
+	    @Override
+	    public String toString()
+	    {
+	        return cityName + " is placed at (" +getX()+", "+getY()+")";
+	    }
+	    
+	    public String toString1()
+	    {
+	        return cityName;
+	    }
+
+}
---
+++ Population.java
@@ -0,0 +1,66 @@
+package tsp02;
+
+public class Population {
+	Circle[] circles;
+
+    public Population(int getSize, boolean initialize) 
+    {
+        circles = new Circle[getSize];
+        
+        if (initialize) 
+        {
+            for (int i = 0; i < getSize(); i++) 
+            {
+                Circle newCircle = new Circle();
+                newCircle.generateIndividual();
+                saveCircle(i, newCircle);
+            }
+        }
+    }
+    
+    // Saves a circle
+    public void saveCircle(int index, Circle circle) 
+    {
+        circles[index] = circle;
+    }
+    
+    //returns circle at given index 
+    public Circle getCircle(int index) 
+    {
+        return circles[index];
+    }
+
+    
+    public Circle getFittest() 
+    {
+        Circle fittest = circles[0];
+        
+        for (int i = 1; i < getSize(); i++) 
+        {
+            if (fittest.getFitness() <= getCircle(i).getFitness()) 
+            {
+                fittest = getCircle(i);
+            }
+        }
+        return fittest;
+    }
+    
+    @Override
+    public String toString()
+    {
+      String out = new String();
+      
+      for(int i = 0; i<getSize(); i++)
+      {
+         out += "\n" + circles[i].toString();
+      }
+      
+      return out;
+    }
+       
+    
+    public int getSize() 
+    {
+        return circles.length;
+    }
+}
